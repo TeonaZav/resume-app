69f939f07360672ca1365d3fde77be03272fe18f
TeonaZav <teona.zavrashvili@gmail.com>
2023-02-13 18:59:44 +0400

    remove unnecessary components

--- src/components/RoutesComponent.js
+++ src/components/RoutesComponent.js
@@ -1,8 +1,6 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "../pages/Home";
 import GeneralInfo from "../pages/GeneralInfo";
-import Experiences from "../pages/Experiences";
-import Education from "../pages/Education";
 import ResumePage from "../pages/ResumePage";
 const RoutesComponent = () => {
   const location = useLocation();
@@ -11,8 +9,7 @@ const RoutesComponent = () => {
     <Routes location={location} key={location.pathname}>
       <Route path="/" exact={true} element={<Home />} />
       <Route path="/general" element={<GeneralInfo />} />
-      <Route path="/experience" element={<Experiences />} />
-      <Route path="/education" element={<Education />} />
+
       <Route path="/resume" element={<ResumePage />} />
     </Routes>
   );
--- src/pages/Education.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Resume from "../components/Resume";
-const Education = () => {
-  return <div>Education</div>;
-};
-
-export default Education;
